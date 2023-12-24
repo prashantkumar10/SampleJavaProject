95e0d84fc8e2f47d6d3ecdd03e4115314b4db447
Prashant Kumar Chauhan <prashantkumarchauhan2000@gmail.com>
2023-12-24 09:17:16 +0530

    Second Commit

--- SampleGitHubJavaProject/src/sample/SampleClass.java
+++ SampleGitHubJavaProject/src/sample/SampleClass.java
@@ -3,6 +3,7 @@ package sample;
 public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Hello This is a Sample Class");
+		System.out.println("Hello This is new changes");
 	}
 
 }
