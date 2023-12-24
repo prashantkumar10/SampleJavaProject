f840cf7fc205cea5bf0bfce2e43d1d1484cbdd6b
Prashant Kumar Chauhan <prashantkumarchauhan2000@gmail.com>
2023-12-24 09:36:43 +0530

    Third Commit

--- SampleGitHubJavaProject/src/sample/SampleClass.java
+++ SampleGitHubJavaProject/src/sample/SampleClass.java
@@ -4,6 +4,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Hello This is a Sample Class");
 		System.out.println("Hello This is new changes");
+		System.out.println("Hello This is third Commit");
 	}
 
 }
