ee51cb1a633999d4ec5bebcc7dd09f6f32ee0ed9
Prashant Kumar Chauhan <prashantkumarchauhan2000@gmail.com>
2023-12-25 09:35:51 +0530

    Changes in the Third branch

--- SampleGitHubJavaProject/src/sample/SampleClass.java
+++ SampleGitHubJavaProject/src/sample/SampleClass.java
@@ -5,7 +5,7 @@ public class SampleClass {
 		System.out.println("Hello This is a Sample Class");
 		System.out.println("Hello This is new changes");
 		System.out.println("This is the changes for the first branch");
-		System.out.println("This is the changes for the second branch");
+		System.out.println("This is the changes for the third branch");
 	}
 
 }
