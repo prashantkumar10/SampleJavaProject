36a036f4004440483a95fca5583437809400d05c
Prashant Kumar Chauhan <prashantkumarchauhan2000@gmail.com>
2023-12-25 09:38:14 +0530

    Changes in the Master branch

--- SampleGitHubJavaProject/src/sample/SampleClass.java
+++ SampleGitHubJavaProject/src/sample/SampleClass.java
@@ -6,6 +6,7 @@ public class SampleClass {
 		System.out.println("Hello This is new changes");
 		System.out.println("This is the changes for the first branch");
 		System.out.println("This is the changes for the second branch");
+		System.out.println("This is the changes in the Master Branch");
 	}
 
 }
