705c0b32e1592dbafc8d22784ccb79a5a339cca0
Prashant Kumar Chauhan <prashantkumarchauhan2000@gmail.com>
2023-12-26 09:51:02 +0530

    Next Commit

--- SampleGitHubJavaProject/src/sample/SampleClass.java
+++ SampleGitHubJavaProject/src/sample/SampleClass.java
@@ -10,6 +10,20 @@ public class SampleClass {
 		System.out.println("This is the second change in the Master branch");
 		System.out.println("This is the changes for the fourth branch");
 		
+		int a=10;
+		int sum=a+10;
+		try {
+			double div=a/0;
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		System.out.println(sum);
+		System.out.println("Hello Prashant");
+		int sub=a-4;
+		System.out.println(sub);
+		
 		
 
 	}
